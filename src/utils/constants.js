48920f88d007dd8ceb21c7ac7b216d1713165c1f
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2024-04-20 21:29:06 +0530

    api call

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmUyYjNmMGJlODBhMmZhZWZlOTllOTRmNmMwOTE4OSIsInN1YiI6IjY2MjNjZjA3YjI2ODFmMDFhOTcyYzZhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Eh3z0ig4tGv2cJD5DfsjDracbiVlEqsmCyv5T9xJ1iw",
+  },
+};
