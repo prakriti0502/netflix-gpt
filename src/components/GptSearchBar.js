fba0a4807aeae90187d228b4f1a46ce9caa9d42f
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2024-05-05 22:34:19 +0530

    done

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
+import openai from "../utils/openai";
 
 const GptSearchBar = () => {
-    const langKey = useSelector(store=>store.config.lang);
+  const langKey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    // Make an API call to GPT API and get movie results.
+
+    // only giving search text would generate random answers from api.
+    // explain a little more, i.e. give a good prompt.
+
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query: " +
+      searchText.current.value +
+      ". Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Don, Pulp Fiction, Sholay, Shawshank Redemption, Scarface";
+
+    // const gptResults = await openai.chat.completions.create({
+    //   messages: [{ role: "user", content: searchText.current.value }],
+    //   model: "gpt-3.5-turbo",
+    // });
+    console.log(gptQuery);
+  };
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="bg-black w-1/2 grid grid-cols-12">
+      <form
+        className="bg-black w-1/2 grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           placeholder={lang[langKey].gptSearchPlaceholder}
           className="p-4 m-4 col-span-9"
         />
-        <button className="col-span-3 m-4 py-2 px-4 rounded-lg bg-red-700 text-white">
+        <button
+          className="col-span-3 m-4 py-2 px-4 rounded-lg bg-red-700 text-white"
+          onClick={handleGptSearchClick}
+        >
           {lang[langKey].search}
         </button>
       </form>
---
+++ src/utils/openai.js
@@ -0,0 +1,11 @@
+// import OpenAI from 'openai';
+// // import { OPENAI_KEY } from './constants';
+
+// const openai = new OpenAI({
+// //   apiKey: OPENAI_KEY,
+//   // by default open ai doesn't allow to make calls from browser as it would expose your secret key.
+//   // so use this flag to surpass that error.
+// //   dangerouslyAllowBrowser: true
+// });
+
+// export default openai;
